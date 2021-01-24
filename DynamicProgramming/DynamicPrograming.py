9a3ba76e1fa8c2115243258cbc121c33b4c285f8
woosub-lee <woosub.project@gmail.com>
2021-01-24 23:07:16 +0900

    add bottom-up DP

--- DynamicProgramming/DynamicPrograming.py
+++ DynamicProgramming/DynamicPrograming.py
@@ -1,13 +1,24 @@
-# 메모제이션 기법 예제(피보나치 수열)
-def fibo(array, number):
+# 다이나믹프로그래밍 예제(피보나치 수열)
+def fiboTD(array, number):  # 탑다운
     if number == 1 or number == 2:
         return 1
     if array[number] != 0:
         return array[number]
-    array[number] = fibo(array, number-1) + fibo(array, number-2)
+    array[number] = fiboTD(array, number-1) + fiboTD(array, number-2)
     return array[number]
 
 
+def fiboBU(array, number):  # 보텀업
+    array[1], array[2] = 1, 1
+    if number > 2:
+        for i in range(3, number+1):
+            array[i] = array[i-1] + array[i-2]
+        return array[i]
+    else:
+        return array[number]
+
 n = int(input())
-memory = [0 for _ in range(n+1)]
-print(fibo(memory, n))
+memoryTD = [0 for _ in range(n+2)]  # 탑다운 방식의 메모제이션 기법을 위한 리스트
+memoryBU = [0 for _ in range(n+2)]  # 보텀업 방식의 DP테이블
+print(fiboTD(memoryTD, n))
+print(fiboBU(memoryBU, n))
