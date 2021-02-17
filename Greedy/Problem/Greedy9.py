39efe50ac85de7e57b300fe9d50060b1f6304532
woosub-lee <woosub.project@gmail.com>
2021-02-17 22:56:59 +0900

    make_problem_directory

--- Greedy/Problem/Greedy9.py
+++ Greedy/Problem/Greedy9.py
@@ -1,6 +1,7 @@
 # 볼링공 고르기
 # 두사람이 서로 다른 무게를 가진 볼링공을 고르기
 # 고를 수 있는 경우의 수 구하기
+
 NumberOfBall, MaximumWeight = map(int, input().split())
 NumberOfCases = 0
 Balls = list(map(int, input().split()))
@@ -12,4 +13,3 @@ for i in range(NumberOfBall-1):
             NumberOfCases += 1
 
 print(NumberOfCases)
-
