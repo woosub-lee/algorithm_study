499b5fcdd51f384e8c11e2c3c62cd57501843992
woosub-lee <woosub.project@gmail.com>
2021-02-17 22:36:18 +0900

    improving_complexity

--- Greedy/Greedy7.py
+++ Greedy/Greedy7.py
@@ -1,4 +1,5 @@
 # 문자열 뒤집기
+"""
 def DataZip(data):
     ZippedData = [data[0]]
     for d in data[1:]:
@@ -11,4 +12,20 @@ OriginalData = list(input())
 Data = DataZip(OriginalData)
 print(Data)
 NumberOfReverse = min(Data.count('0'), Data.count('1'))
+"""
+StrData = input()
+NumberOfCase0 = 0
+NumberOfCase1 = 0
+NumberOfReverse = 0
+for i in range(1, len(StrData)):
+    if StrData[i-1] != StrData[i]:
+        if StrData[i] == '1':
+            NumberOfCase0 += 1
+        else:
+            NumberOfCase1 += 1
+
+NumberOfReverse = min(NumberOfCase0, NumberOfCase1)
 print(NumberOfReverse)
+
+
+
