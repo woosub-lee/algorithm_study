5654be0e54249b4d66a6565411a0a67ce0cf3604
woosub-lee <woosub.project@gmail.com>
2021-02-28 21:26:39 +0900

    studying_code

--- Implementation/Problem/Implementation11.py
+++ Implementation/Problem/Implementation11.py
@@ -1,9 +1,11 @@
 # 외벽 점검
 # 외부 코드에서 n, weak, dist를 제공받음 (https://programmers.co.kr/learn/courses/30/lessons/60062 참고)
 """
-실수했던점:
+실수했던 점: -1을 반환하는 코드를 생각하지 못했다.
+           예외처리, 종료조건등을 확실히 하자.
 
-기억해야할 점:
+기억해야할 점: permutations 를 임포트해 순열을 구할 수 있었다.
+             2바퀴 어치 리스트를 만들어 구현을 간단히 할 수 있었다.
 """
 
 from itertools import permutations
